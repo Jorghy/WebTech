ab3a4c38eeb65aa18fd7386cf212ed51fa7afabc
Jorghy <36571093+Jorghy@users.noreply.github.com>
2019-11-30 16:22:11 +0100

    Ajax
    Adatok küldése AJAX technológiával.

--- Beadandó/student/JavaScript/index.js
+++ Beadandó/student/JavaScript/index.js
@@ -39,7 +39,7 @@ function getManufacturerTable() {
             var man_data = '';
             $.each(manufacturers, function(key, value) {
                 man_data += "<tr>";
-                man_data += "<td>"+value.name+"</td>";
+                man_data += "<td><a id="+value.name+" onclick='getCarByManufacturer(this.id)'>"+value.name+"</a></td>";
                 man_data += "<td>"+value.country+"</td>";
                 man_data += "<td>"+value.founded+"</td>";
                 man_data += "</tr>"; 
@@ -57,3 +57,50 @@ function getManufacturerName() {
         $("#manufacturer").append(man_names);
     });
 }
+
+function getCarByManufacturer(id) {
+    document.cookie = "name = " + String.empty;
+    document.cookie = "name = " + id;
+    $('#carByManu_table').children().remove();
+    var th = "<tr><th>Name</th><th>Consumption</th><th>Color</th><th>Manufacturer</th><th>Available</th><th>Year</th><th>Horsepower</th></tr>";
+    $('#carByManu_table').append(th);
+    $.getJSON("/manufacturer", function(c) {
+        var car_data = '';
+        $.each(c, function(key, value) {
+            car_data += "<tr>";
+            car_data += "<td>"+value.name+"</td>";
+            car_data += "<td>"+value.consumption+"</td>";
+            car_data += "<td>"+value.color+"</td>";
+            car_data += "<td>"+value.manufacturer+"</td>";
+            car_data += "<td>"+value.available+"</td>";
+            car_data += "<td>"+value.year+"</td>";
+            car_data += "<td>"+value.horsepower+"</td>";
+            car_data += "</tr>"; 
+        });
+        $('#carByManu_table').append(car_data);
+    });
+}
+
+$(function() {
+    var form = $('#addCar');
+    $(form).submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: form.attr('action'),
+        data: form.serialize()
+        })
+    });
+});
+
+$(function() {
+    var form = $('#addManu');
+    $(form).submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: form.attr('action'),
+        data: form.serialize()
+        })
+    });
+});
